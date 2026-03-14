23bc5152e52bb1a7a8aeec93ada36ff87f24b1d3
rochi <rochi.sigler@gmail.com>
2026-03-14 12:49:37 -0300

    fetch data and map

--- src/organisms/map/Map.jsx
+++ src/organisms/map/Map.jsx
@@ -1,8 +1,25 @@
-
+import 'leaflet/dist/leaflet.css'
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 const Map = () => {
   return (
-    <div>Map</div>
+
+    <MapContainer
+      center={[-34.6037, -58.3816]}
+      zoom={13}
+      style={{ height: "500px", width: "100%" }}
+    >
+      <TileLayer
+        attribution='&copy; OpenStreetMap contributors'
+        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[-34.6037, -58.3816]} >
+        <Popup>
+          Buenos Aires
+        </Popup>
+      </Marker>
+    </MapContainer>
+
   )
 }
 
--- src/templates/R01IpAdressTracker/R01IpAddressTracker.jsx
+++ src/templates/R01IpAdressTracker/R01IpAddressTracker.jsx
@@ -1,13 +1,49 @@
 import AddressInformation from "../../organisms/addressInformation/AddressInformation"
 import Map from "../../organisms/map/Map"
 import SearchBar from "../../organisms/searchBar/SearchBar"
+import { useState, useEffect } from 'react'
+
 
 const R01IpAddressTracker = () => {
+  const [data, setData] = useState(null)
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://geo.ipify.org/api/v2/country?apiKey=at_ZCr63iuS0xJLtPlWX14PBRY5CVse1')
+        if (!response.ok) {
+          throw new Error(`HTTP error: Status ${response.status}`)
+        }
+        const result = await response.json()
+        setData(result)
+        setError(null)
+      } catch (error) {
+        setError(error.message)
+        setData(null)
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchData()
+  }, [])
+
+  console.log(data.ip)
+  if (loading) {
+    return <p>Loading...</p>
+  }
+  if (error) {
+    return <p>Error: {error}</p>
+  }
+
+
+
   return (
     <div>
-        <SearchBar />
-        <AddressInformation />
-        <Map />
+      <SearchBar />
+      <AddressInformation />
+      <Map />
     </div>
   )
 }
